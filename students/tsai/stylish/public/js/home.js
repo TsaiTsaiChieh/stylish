491b2d3ac981749d285c998a72acea4bda4d25df
Tsai, Tsai-Chieh <jecica196@gmail.com>
2019-07-24 17:26:40 +0800

    Fix the screen min-width to 1200px

--- students/tsai/stylish/public/js/home.js
+++ students/tsai/stylish/public/js/home.js
@@ -1,73 +1,108 @@
 const product_container = document.getElementsByClassName('product_container')[0];
+const carousel = document.getElementsByClassName('carousel')[0];
+attachBannerList(carousel);
+attachProductList(product_container);
 
-{/* <div class="card">
-                <img src="../uploads/main_1563069871770.jpg" class="card-img-top">
-                <div class="card-body">
-                    <div class="colors">
-                        <div class="color"></div>
-                        <div class="color"></div>
-                    </div>
-                    <p class="product-name">前開衩扭結洋裝</p>
-                    <p class="price">TWD. 799</p>
-                </div>
-            </div> */}
-var req = new XMLHttpRequest();
-req.onreadystatechange = function () {
-    if (req.readyState === 4) {
-        var res = JSON.parse(req.responseText);
-        var paging = res.paging;
-        // console.log('size:', req.responseText.data[0].length);
+function attachBannerList(carousel) {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            var res = JSON.parse(req.responseText);
+            let carousel_indicators = document.createElement('ol');
+            carousel_indicators.className = 'carousel-indicators';
+            let carousel_inner = document.createElement('div');
+            carousel_inner.className = 'carousel-inner';
 
+            for (let i = 0; i < res.data.length; i++) {
+                let carouselExampleCaptions = document.createElement('li');
+                carouselExampleCaptions.setAttribute('data-target', '#carouselExampleCaptions');
+                carouselExampleCaptions.setAttribute('data-slide-to', i);
+                carousel_indicators.appendChild(carouselExampleCaptions);
 
-        for (let i = 0; i < Object.keys(res.data).length; i++) {
+                let carousel_item = document.createElement('div');
+                carousel_item.setAttribute('class', 'carousel-item');
+                if (i == 0) {
+                    carousel_item.setAttribute('class', 'carousel-item active');
+                }
+                carousel_inner.appendChild(carousel_item);
 
-            console.log('data:', res.data[i].images[0]);
-            let card = document.createElement('div');
-            card.className = 'card';
+                let banner_img = document.createElement('img');
+                banner_img.setAttribute('class', 'd-block w-100');
+                banner_img.src = '../campaigns/' + res.data[i].picture;
+                carousel_item.appendChild(banner_img);
 
-            let card_img_top = document.createElement('img');
-            card_img_top.className = 'card-img-top';
-            card_img_top.src = '../uploads/' + res.data[i].main_image;
+                let carousel_caption = document.createElement('div');
+                carousel_caption.className = 'carousel-caption';
+                carousel_item.appendChild(carousel_caption);
 
-            let card_body = document.createElement('div');
-            card_body.className = 'card-body';
-            // color
-            let colors = document.createElement('div');
-            colors.className = 'colors';
-            for (let j = 0; j < res.data[i].colors.length; j++) {
+                let full_width = document.createElement('div');
+                full_width.className = 'full-width';
+                carousel_caption.appendChild(full_width);
 
-                let color = document.createElement('div');
-                color.className = 'color';
-                color.style.background = '#' + res.data[i].colors[j].code;
-                colors.appendChild(color);
+                let h4 = document.createElement('h4');
+
+                let story_array = res.data[i].story.split(" ");
+                var temp = '';
+                for (let k = 0; k < story_array.length; k++) {
+                    temp += story_array[k] + '<br>';
+                }
+                h4.innerHTML = temp;
+                full_width.appendChild(h4);
             }
-            card_body.appendChild(colors);
-            let product_name = document.createElement('p');
-            product_name.className = 'product-name';
-            product_name.innerHTML = res.data[i].title;
-            let price = document.createElement('p');
-            price.className = 'price';
-            price.innerHTML = 'TWD. ' + res.data[i].price;
-            card_body.appendChild(product_name);
-            card_body.appendChild(price);
-            card.appendChild(card_img_top);
-            card.appendChild(card_body);
-            product_container.appendChild(card);
+            carousel.appendChild(carousel_indicators);
+            carousel.appendChild(carousel_inner);
         }
-
-        // document.getElementsByClassName('test')[0].innerHTML = res.data;
-        console.log(res.data);
-
-
-
-
-
     }
-};
-req.open('get', '/api/1/products/all');
-req.send();
-// function attachProductList(product_container) {
+    req.open('get', '/api/1/marketing/campaigns');
+    req.send();
+}
+function attachProductList(product_container) {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            var res = JSON.parse(req.responseText);
+            // var paging = res.paging;
 
-// }
+            for (let i = 0; i < Object.keys(res.data).length; i++) {
+                let card = document.createElement('div');
+                card.className = 'card';
+
+                let card_img_top = document.createElement('img');
+                card_img_top.className = 'card-img-top';
+                card_img_top.src = '../uploads/' + res.data[i].main_image;
+
+                let card_body = document.createElement('div');
+                card_body.className = 'card-body';
+                // color
+                let colors = document.createElement('div');
+                colors.className = 'colors';
+                for (let j = 0; j < res.data[i].colors.length; j++) {
+
+                    let color = document.createElement('div');
+                    color.className = 'color';
+                    color.style.background = '#' + res.data[i].colors[j].code;
+                    colors.appendChild(color);
+                }
+                card_body.appendChild(colors);
+                let product_name = document.createElement('p');
+                product_name.className = 'product-name';
+                product_name.innerHTML = res.data[i].title;
+                let price = document.createElement('p');
+                price.className = 'price';
+                price.innerHTML = 'TWD. ' + res.data[i].price;
+                card_body.appendChild(product_name);
+                card_body.appendChild(price);
+                card.appendChild(card_img_top);
+                card.appendChild(card_body);
+                product_container.appendChild(card);
+            }
+        }
+    };
+    req.open('get', '/api/1/products/all');
+    req.send();
+}
+
+
+
 
 
