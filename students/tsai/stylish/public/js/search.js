e9a6d3beedbbea51fc18f713f177ea8deafd34a5
Tsai, Tsai-Chieh <jecica196@gmail.com>
2019-07-31 17:12:55 +0800

    add search function and fix the carousel bug

--- students/tsai/stylish/public/js/search.js
+++ students/tsai/stylish/public/js/search.js
@@ -8,3 +8,18 @@ const search_btn = document.getElementsByClassName('search-btn')[0];
 search_btn.addEventListener('click', function (e) {
     window.location.href = `./?category=search&keyword=${form_control.value}&paging=0`;
 });
+let search_click = document.getElementsByClassName('search_click')[0];
+
+if (search_click) {
+    search_click.addEventListener('click', function (e) {
+        let mobile_search = document.getElementsByClassName('mobile_search')[0];
+        let search = document.getElementsByClassName('search')[0];
+        mobile_search.style.display = 'block';
+        search_click.style.display = 'none';
+        search.focus();
+        search.addEventListener('blur', function (e) {
+            mobile_search.style.display = 'none';
+            search_click.style.display = 'block';
+        });
+    });
+}
