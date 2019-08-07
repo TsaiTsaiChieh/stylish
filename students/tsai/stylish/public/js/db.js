7325f82de1be21267d23761811c94b07423adec0
Tsai, Tsai-Chieh <jecica196@gmail.com>
2019-08-07 10:55:58 +0800

    using mysql pool

--- students/tsai/stylish/public/js/db.js
+++ students/tsai/stylish/public/js/db.js
@@ -1,17 +1,28 @@
 const mysql = require('mysql'); //npm install mysql
 // database
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "0000",
+//     database: "stylish"
+// });
+const db = mysql.createPool({
+    connectionLimit: 100,
     host: "localhost",
     user: "root",
     password: "0000",
-    database: "stylish"
+    database: "stylish",
+    /* optional */
+    acquireTimeout: 10000,
+    waitForConnections: true,
+    queueLimit: 5
 });
 
-db.connect(function (err) {
-    if (err) {
-        throw err;
-    }
-    console.log("stylish connected!");
-});
+// db.connect(function (err) {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log("stylish connected!");
+// });
 
 module.exports = db;
